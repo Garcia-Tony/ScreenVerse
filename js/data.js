f7aa38221dcf776334b6ddf7180b104117bc76d5
Anthony Garcia <166793944+Garcia-Tony@users.noreply.github.com>
2025-04-29 01:54:15 +0000

    done

--- js/data.js
+++ js/data.js
@@ -1,17 +1,16 @@
-
-"use strict";
+'use strict';
 const dataKey = 'movie-search-data';
 const data = readData();
 function readData() {
-    const localData = localStorage.getItem(dataKey);
-    if (localData) {
-        return JSON.parse(localData);
-    }
-    return {
-        view: 'search-form',
-        movies: [],
-    };
+  const localData = localStorage.getItem(dataKey);
+  if (localData) {
+    return JSON.parse(localData);
+  }
+  return {
+    view: 'search-form',
+    movies: [],
+  };
 }
 function writeData() {
-    localStorage.setItem(dataKey, JSON.stringify(data));
+  localStorage.setItem(dataKey, JSON.stringify(data));
 }
